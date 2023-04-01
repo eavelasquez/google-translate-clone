03ff9c14100975b746a82b4d58172a2b2d6084e2
eavelasquez <eavc.30@gmail.com>
2023-04-01 18:52:01 -0500

    feat: add support for new languages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,9 @@ export const SUPPORTED_LANGUAGES = {
   en: 'English',
   es: 'Español',
   fr: 'Français',
-  de: 'Deutsch'
+  de: 'Deutsch',
+  it: 'Italiano',
+  ja: '日本語'
 }
 
 export const AUTO_DETECT_LANGUAGE = 'auto'
