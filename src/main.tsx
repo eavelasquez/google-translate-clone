7a99615310e139dcbde26e91c0c414f77df31660
eavelasquez <eavc.30@gmail.com>
2023-03-31 22:28:50 -0500

    feat: install react-bootstrap and bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
