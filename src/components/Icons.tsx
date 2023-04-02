9aaefeb0c4e5402b99b2ea863cb2a6059d172c8a
eavelasquez <eavc.30@gmail.com>
2023-04-02 12:22:03 -0500

    feat: update icons attributes to use single quotes

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -5,17 +5,17 @@ export const ArrowsIcon = () => (
 )
 
 export const ClipboardIcon = () => (
-  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24">
+  <svg focusable='false' xmlns='http://www.w3.org/2000/svg' enableBackground='new 0 0 24 24' height='24' viewBox='0 0 24 24' width='24'>
     <g>
-      <rect fill="none" height="24" width="24"></rect>
+      <rect fill='none' height='24' width='24'></rect>
       </g><g>
-      <path d="M16,20H5V6H3v14c0,1.1,0.9,2,2,2h11V20z M20,16V4c0-1.1-0.9-2-2-2H9C7.9,2,7,2.9,7,4v12c0,1.1,0.9,2,2,2h9 C19.1,18,20,17.1,20,16z M18,16H9V4h9V16z"></path>
+      <path d='M16,20H5V6H3v14c0,1.1,0.9,2,2,2h11V20z M20,16V4c0-1.1-0.9-2-2-2H9C7.9,2,7,2.9,7,4v12c0,1.1,0.9,2,2,2h9 C19.1,18,20,17.1,20,16z M18,16H9V4h9V16z'></path>
     </g>
   </svg>
 )
 
 export const SpeakerIcon = () => (
-  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" height='24' viewBox="0 0 24 24" width='24'>
-    <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
+  <svg focusable='false' xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 0 24 24' width='24'>
+    <path d='M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z'></path>
   </svg>
 )
