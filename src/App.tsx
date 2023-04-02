30dabe5853f548557476b13946cfdbf70706b5f2
eavelasquez <eavc.30@gmail.com>
2023-04-02 11:37:22 -0500

    feat: add tooltips to the clipboard button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Button, Col, Container, Row } from 'react-bootstrap'
-import { useEffect } from 'react'
+import { Button, Col, Container, Overlay, OverlayTrigger, Row, Tooltip } from 'react-bootstrap'
+import { useEffect, useRef, useState } from 'react'
 
 import { ArrowsIcon, LanguageSelector, TextArea } from './components'
 import { AUTO_DETECT_LANGUAGE } from './utils/constants'
 import { SectionType } from './types.d'
 import { translate } from './services/translate'
 import { useDebounce, useStore } from './hooks'
+import { ClipboardIcon } from './components/Icons'
 
 function App () {
+  const [showClipboardTooltip, setShowClipboardTooltip] = useState(false)
+  const targetClipboard = useRef(null)
+
   const {
     fromLanguage,
     toLanguage,
@@ -35,6 +39,11 @@ function App () {
         setTranslatedText('Something went wrong')
       })
   }, [fromLanguage, toLanguage, debouncedText])
+
+  const handleClipboardClick = () => {
+    setShowClipboardTooltip(!showClipboardTooltip)
+    void navigator.clipboard.writeText(translatedText)
+  }
 
   return (
     <Container fluid>
@@ -64,12 +73,36 @@ function App () {
 
         <Col xs={12} md={5}>
           <LanguageSelector type={SectionType.To} value={toLanguage} onChange={setToLanguage} />
-          <TextArea
-            type={SectionType.To}
-            loading={loading}
-            text={translatedText}
-            onChange={setTranslatedText}
-          />
+
+          <div className='clipboard-icon'>
+            <TextArea
+              type={SectionType.To}
+              loading={loading}
+              text={translatedText}
+              onChange={setTranslatedText}
+            />
+            <OverlayTrigger
+              key={'right'}
+              placement={'right'}
+              overlay={
+                <Tooltip id={'tooltip-right'}>
+                  Copy to clipboard
+                </Tooltip>
+              }
+              rootClose={true}
+            >
+              <Button ref={targetClipboard} variant='link' disabled={loading} onClick={handleClipboardClick}>
+                <ClipboardIcon />
+              </Button>
+            </OverlayTrigger>
+            <Overlay target={targetClipboard.current} show={showClipboardTooltip} placement='bottom' rootClose={true} onHide={() => { setShowClipboardTooltip(false) }}>
+              {(props) => (
+                <Tooltip id='overlay-cliboard' {...props}>
+                  Copied to clipboard
+                </Tooltip>
+              )}
+            </Overlay>
+          </div>
         </Col>
       </Row>
     </Container>
