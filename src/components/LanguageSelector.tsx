50a60e759acb879cb31c715a04e37bda7e1a919b
eavelasquez <eavc.30@gmail.com>
2023-04-01 20:48:52 -0500

    feat: add auto detect language option

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,23 +1,21 @@
 import { Form } from 'react-bootstrap'
 import { type ChangeEvent, type FC } from 'react'
 
-import { SUPPORTED_LANGUAGES } from '../utils/constants'
+import { AUTO_DETECT_LANGUAGE, SUPPORTED_LANGUAGES } from '../utils/constants'
 import { type FromLanguage, type Language } from '../types'
 
 type Props =
   | { type: 'from', value: FromLanguage, onChange: (language: FromLanguage) => void }
   | { type: 'to', value: Language, onChange: (language: Language) => void }
 
-export const LanguageSelector: FC<Props> = ({ onChange }) => {
+export const LanguageSelector: FC<Props> = ({ type, value, onChange }) => {
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     onChange(event.target.value as Language)
   }
 
   return (
-    <Form.Select
-      aria-label='Select Language'
-      onChange={handleChange}
-    >
+    <Form.Select aria-label='Select Language' onChange={handleChange} size='sm' value={value}>
+      {type === 'from' && <option value={AUTO_DETECT_LANGUAGE}>Detect language</option>}
       {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
         <option key={key} value={key}>
           {literal}
