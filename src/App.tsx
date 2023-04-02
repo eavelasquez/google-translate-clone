b970b43e9626e84b9bf7001ed269fa624f2ce2e5
eavelasquez <eavc.30@gmail.com>
2023-04-01 20:38:27 -0500

    feat: add language selector component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { Button, Col, Container, Row } from 'react-bootstrap'
 import { useStore } from './hooks/useStore'
 import { AUTO_DETECT_LANGUAGE } from './utils/constants'
 import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
 
 function App () {
-  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
+  const { fromLanguage, toLanguage, interchangeLanguages, setFromLanguage, setToLanguage } = useStore()
 
   return (
     <Container fluid>
       <h1>Google Translate</h1>
       <Row>
         <Col>
-          <h2>From Language</h2>
-          <p>{fromLanguage}</p>
+          <LanguageSelector type='from' value={fromLanguage} onChange={setFromLanguage} />
         </Col>
 
         <Col>
@@ -27,8 +27,7 @@ function App () {
         </Col>
 
         <Col>
-          <h2>To Language</h2>
-          <p>{toLanguage}</p>
+          <LanguageSelector type='to' value={toLanguage} onChange={setToLanguage} />
         </Col>
       </Row>
     </Container>
