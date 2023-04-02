b44fdb37519f03fb5b3a4d9f35fcfb672957da8c
eavelasquez <eavc.30@gmail.com>
2023-04-02 11:19:08 -0500

    feat: add a new component called clipboard icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,15 @@
 export const ArrowsIcon = () => (
-  <svg focusable='false' width='24' height='24' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+  <svg focusable='false' xmlns='http://www.w3.org/2000/svg' height='24' viewBox='0 0 24 24' width='24'>
     <path d='M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z'></path>
+  </svg>
+)
+
+export const ClipboardIcon = () => (
+  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24">
+    <g>
+      <rect fill="none" height="24" width="24"></rect>
+      </g><g>
+      <path d="M16,20H5V6H3v14c0,1.1,0.9,2,2,2h11V20z M20,16V4c0-1.1-0.9-2-2-2H9C7.9,2,7,2.9,7,4v12c0,1.1,0.9,2,2,2h9 C19.1,18,20,17.1,20,16z M18,16H9V4h9V16z"></path>
+    </g>
   </svg>
 )
